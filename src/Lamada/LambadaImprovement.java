50288828016e460b3b350624a226628654ae05cb
yichunzhao <zhaoyichun@hotmail.com>
2017-08-24 23:31:37 +0200

    Lambada, stream, filter, map, reduce

---
+++ src/Lamada/LambadaImprovement.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lamada;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import javax.sound.midi.Soundbank;
+
+/**
+ *
+ * @author YNZ
+ */
+public class LambadaImprovement {
+
+    public static void main(String[] args) {
+        List<BigDecimal> prices = Arrays.asList(
+                new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+                new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+                new BigDecimal("45"), new BigDecimal("12")
+        );
+
+        BigDecimal totalOfDiscountedPrices = prices.stream().filter(
+                price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+                .map(price->price.multiply(BigDecimal.valueOf(0.9)))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        
+        System.out.println("totalOfDiscountedPrices = " + totalOfDiscountedPrices);
+
+    }
+
+}
