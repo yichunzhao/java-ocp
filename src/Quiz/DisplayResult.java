76268dc4d0895cebbc63f2b3818a70499a05a932
yichunzhao <zhaoyichun@hotmail.com>
2020-07-24 23:49:46 +0200

    code quiz

---
+++ src/Quiz/DisplayResult.java
@@ -0,0 +1,14 @@
+package Quiz;
+
+public class DisplayResult {
+    private boolean flag;
+    public void displayResult(){
+        int result=flag?5:10;
+        System.out.println("Result="+ result++);
+    }
+
+    public static void main(String[] args) {
+        new DisplayResult().displayResult();
+
+    }
+}
---
+++ src/Quiz/IfBlock.java
@@ -0,0 +1,14 @@
+package Quiz;
+
+public class IfBlock {
+
+
+    public static void main(String[] args) {
+        boolean myVal = false;
+        if (myVal)
+            for (int i = 0; i < 2; i++) System.out.println(i);
+
+        else System.out.println("else");
+
+    }
+}
