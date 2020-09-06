e4814218216061292334d230c30ce8b4c560245e
yichunzhao <zhaoyichun@hotmail.com>
2020-09-06 13:29:13 +0200

    LocalTime

--- src/Array/ShuffleArray.java
+++ src/Array/ShuffleArray.java
@@ -4,15 +4,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class ShuffleArray {
-  public static void main(String[] args) {
-    // assume we have an array of int in a sequence, and shuffle it, and see what happened.
-    Integer[] ints = {1, 2, 3, 4, 5, 6, 7};
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(ints));
 
-    List<Integer> shuffled = Arrays.asList(ints);
+        //boxing an array of primitives
+        List<Integer> integerList = Arrays.stream(ints).boxed().collect(toList());
+        System.out.println(integerList);
 
-    Collections.shuffle(shuffled);
-
-    System.out.println(shuffled);
-  }
+        //shuffle only a List of Instances
+        Collections.shuffle(integerList);
+        System.out.println("shuffled ints: " + integerList);
+    }
 }
--- src/DateAndLocale/JavaTime.java
+++ src/DateAndLocale/JavaTime.java
@@ -1,33 +1,67 @@
 /*
- * Java 8 new time pack.
+ * Java 8 Time
  *
  *
  */
 package DateAndLocale;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Date;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.util.stream.IntStream;
 
-/** @author YNZ */
+/**
+ * LocalTime, ChronoUnit, Duration.
+ *
+ * @author YNZ
+ */
 public class JavaTime {
 
-  /** @param args the command line arguments */
-  public static void main(String[] args) {
-    LocalDate date = LocalDate.now();
-    System.out.println("" + date);
+    private static int sum = 0;
 
-    LocalDate dateAgain = LocalDate.now();
-    System.out.println("" + dateAgain);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //constructing a time
+        System.out.println("" + LocalTime.now());
 
-    System.out.println("" + (date.compareTo(dateAgain) == 0));
+        //create from a factory method
+        System.out.println("" + LocalTime.of(10, 20, 30));
 
-    Date myDate = new Date();
-    System.out.println("myDate: " + myDate);
+        //parsing a String
+        System.out.println("parsed time:" + LocalTime.parse("20:30:20"));
 
-    //create a local date time by a factory method
-    LocalDateTime past = LocalDateTime.of(2018, Month.FEBRUARY,9,12,13,25,190);
-    System.out.println(past.toString());
-  }
+        System.out.println(LocalTime.of(12, 0).equals(LocalTime.of(12, 0)));
+        System.out.println("hashcode :");
+        System.out.println(LocalTime.of(12, 0).hashCode() == LocalTime.of(12, 0).hashCode());
+
+        System.out.println(LocalTime.parse("06:30").plus(1, ChronoUnit.MINUTES).isAfter(LocalTime.parse("06:30")));
+
+        System.out.println("same time pointing to the same instance?");
+        System.out.println(LocalTime.of(12, 0) == LocalTime.of(12, 0));
+        System.out.println(LocalTime.of(14, 50) == LocalTime.of(14, 50));
+        System.out.println(LocalTime.of(13, 0) == LocalTime.of(13, 0));
+
+        //create a local Time and format it.
+        System.out.println("ISO Local Time: " + LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+        System.out.println("ISO Time: " + LocalTime.now().format(DateTimeFormatter.ISO_TIME));
+
+        //Duration Class for dealing with Time between Two Times; only support SECONDS and NANO
+        Duration duration = Duration.between(LocalTime.of(20, 10), LocalTime.of(23, 10));
+        System.out.println("Duration supports Seconds alone: " + duration.get(ChronoUnit.SECONDS));
+
+        //Using Duration to calculate time elapsed
+        Temporal start = Instant.now();
+
+        IntStream.range(0, 1_000_000).forEach(i -> sum += i);
+
+        Temporal end = Instant.now();
+        System.out.println("time used: " + Duration.between(start, end).toMillis());
+
+    }
+
 }
