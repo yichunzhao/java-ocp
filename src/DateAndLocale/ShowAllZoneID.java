40ecb4f1e5f005ffbcdef9bc2e7e8ddae99b0b23
yichunzhao <zhaoyichun@hotmail.com>
2020-09-07 12:29:42 +0200

    ZoneOffSet map to its ZoneIds

--- src/DateAndLocale/ShowAllZoneID.java
+++ src/DateAndLocale/ShowAllZoneID.java
@@ -3,22 +3,25 @@ package DateAndLocale;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ShowAllZoneID {
 
     public static void main(String[] args) {
-        //All ZoneIds
+        System.out.println("+++++++++++++++++ All ZoneId(String) +++++++++++++++++++++++++++++++++");
+
         System.out.println(
                 ZoneId.getAvailableZoneIds()
                         .stream()
-                        .collect(Collectors
-                                .joining(" ", "++", "++"))
+                        .collect(Collectors.joining(" ", "++", "++"))
         );
 
 
         //ZoneOffset map to ZoneId(there are keys overwritten.)
+        System.out.println("+++++++++++++++++ ZoneOffSet map to ZoneId(String) +++++++++++++++++++");
+
         Map<ZoneOffset, String> zoneOffsetMap = ZoneId.getAvailableZoneIds().stream()
                 .collect(Collectors.toMap(
                         s -> ZonedDateTime.now(ZoneId.of(s)).getOffset(),
@@ -27,6 +30,16 @@ public class ShowAllZoneID {
                 );
 
         zoneOffsetMap.entrySet().forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
+
+        Map<ZoneOffset, List<ZonedDateTime>> zoneOffsetListMap = ZoneId.getAvailableZoneIds().stream()
+                .map(s -> ZonedDateTime.now(ZoneId.of(s)))
+                .collect(Collectors.groupingBy(zonedDateTime -> zonedDateTime.getOffset()));
+
+        System.out.println("#################### ZoneOffSet map to its ZoneIds ######################");
+
+        zoneOffsetListMap.forEach((zoneOffset, zonedDateTimes) ->
+                System.out.println(zoneOffset + "\n" + zonedDateTimes.stream().map(z -> z.getZone().getId())
+                        .collect(Collectors.joining(" & "))));
 
     }
 }
