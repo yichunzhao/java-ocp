c8703059d0f319b4d7e7ff073057b0cc9170edde
yichunzhao <zhaoyichun@hotmail.com>
2020-09-08 18:25:36 +0200

    from String to ZonedDateTime

--- src/DateAndLocale/ZonedDateTimeDemo.java
+++ src/DateAndLocale/ZonedDateTimeDemo.java
@@ -1,5 +1,6 @@
 package DateAndLocale;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -17,17 +18,16 @@ public class ZonedDateTimeDemo {
         System.out.println("current Shanghai time");
         System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Shanghai")));
 
-
-        //ZonedDateTime to String
+        System.out.println("............... From ZonedDateTime to String .....................");
         //showing the time zone offset, we can use the formatter "Z" or "X"
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss Z");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss Z X ");
 
         ZonedDateTime copenhagenTime = ZonedDateTime.now();
         System.out.println("without format:" +copenhagenTime);
 
         System.out.println("formatted: " + formatter.format(copenhagenTime));
 
-        //showing the time zone name
+        //showing the time zone name small z showing zone id(name in a String)
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss z");
         System.out.println("formatted 2: " + formatter2.format(copenhagenTime));
 
@@ -35,6 +35,22 @@ public class ZonedDateTimeDemo {
         DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss Zz");
         System.out.println("formatted 3: " + formatter3.format(copenhagenTime));
 
-        
+        System.out.println("............... From String to ZonedDateTime .....................");
+        ZonedDateTime parsed = ZonedDateTime.parse("2020-09-08T12:18:34.114Z[UTC]");
+        ZonedDateTime parsed1 = ZonedDateTime.parse("2020-09-08T12:18:34.114Z[Asia/Shanghai]");
+
+        System.out.println(parsed);
+        System.out.println(parsed1);
+
+        ZonedDateTime parsed2 =
+                ZonedDateTime.parse("09/08/2020 12:18:34 Asia/Shanghai",
+                        DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss z"));
+        System.out.println(parsed2.format(DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss X")));
+
+        System.out.println("............... From String to LocalDateTime, then Zoned .....................");
+
+        ZonedDateTime zonedLocalDateTime = LocalDateTime.parse("2008-11-01T10:29:20").atZone(ZoneId.systemDefault());
+        System.out.println("Zoned local datetime: " + zonedLocalDateTime.format(DateTimeFormatter.ISO_OFFSET_TIME));
+
     }
 }
