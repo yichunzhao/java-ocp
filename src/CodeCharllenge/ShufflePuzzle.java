e7d488678d804fe796550a58cdb1b73ceb8f8198
yichunzhao <zhaoyichun@hotmail.com>
2020-09-09 16:30:06 +0200

    review

--- src/CodeCharllenge/ShufflePuzzle.java
+++ src/CodeCharllenge/ShufflePuzzle.java
@@ -3,18 +3,17 @@ package CodeCharllenge;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-//HOW TO SOLVE 8-PUZZLE ISSUE?
 
+//HOW TO SOLVE 8-PUZZLE ISSUE?
+//Collections service function: shuffle a List; only a List can be shuffled.
 public class ShufflePuzzle {
 
-  public static void main(String[] args) {
-    //
-    String[] x ={"1","2","3","4","5","6","7","None"};
-    List<String> shuffled = Arrays.asList(x);
-    Collections.shuffle(shuffled);
+    public static void main(String[] args) {
+        //convert to a List
+        String[] x = {"1", "2", "3", "4", "5", "6", "7", "None"};
+        List<String> shuffled = Arrays.asList(x);
+        Collections.shuffle(shuffled);
 
-    System.out.println(shuffled);
-
-
-  }
+        System.out.println(shuffled);
+    }
 }
--- src/Collection/ConvertListToSet.java
+++ src/Collection/ConvertListToSet.java
@@ -5,24 +5,35 @@
  */
 package Collection;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-/** @author YNZ */
+/**
+ * @author YNZ
+ */
 public class ConvertListToSet {
 
-  public static void main(String[] args) {
-    Stream<String> strs = Stream.of("wo", "he", "ni", "xin", "lian", "xin");
-    List<String> strList = strs.collect(Collectors.toList());
+    public static void main(String[] args) {
+        List<String> strList = Arrays.asList("wo", "he", "ni", "xin", "lian", "xin");
+        System.out.println("original list of String:" + strList);
 
-    // convert to set
-    Set<String> strSet = new HashSet<>(strList);
+        // convert a List to Set using Set constructor
+        Set<String> strSet = new HashSet<>(strList);
+        System.out.println("set : " + strSet);
 
+        Set<String> strTreeSet = new TreeSet<>(strList);
+        System.out.println("tree set : " + strTreeSet);
 
+        // collect the List into a Set
+        Set<String> strSet2 = strList.stream().collect(Collectors.toSet());
+        System.out.println("collect from a stream: " + strSet2);
 
-    System.out.println("set : " + strSet);
-  }
+        Set<String> strTreeSet2 = strList.stream().collect(Collectors.toCollection(TreeSet::new));
+        System.out.println("collect from a stream: " + strTreeSet2);
+
+    }
 }
