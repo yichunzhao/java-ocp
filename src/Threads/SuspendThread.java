ddba4c074ad55d8b7c9a0157fbb9d5f54b5b1096
yichunzhao <zhaoyichun@hotmail.com>
2020-09-19 00:16:33 +0200

    demonstrating a blocking Queue, using Object wait() and notify()

--- src/Threads/SuspendThread.java
+++ src/Threads/SuspendThread.java
@@ -4,9 +4,12 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /**
+ * How to synchronize two group of threads to carry out task in a sequence.
+ * <p>
  * to access a protected resource, a thread need to achieve its monitor first.
  * <p>
  * one object has a monitor; once object holds his monitor, all external threads that intend to access the object, then
@@ -16,20 +19,35 @@ import java.util.concurrent.ThreadPoolExecutor;
  * notify(): wake up threads that are waiting for this object's monitor.
  * wait(): causes current thread that own the object monitor to give up the monitor;
  */
+class Event {
+    private String title;
+
+    public Event(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
 
 class DataStore {
     public static final int qSize = 20;
-    private Queue<Integer> q = new ArrayDeque<>(qSize);
+    private Queue<Event> q = new ArrayDeque<>(qSize);
 
-    synchronized public void insert(Integer integer) {
-        //if the queue has a certain amount of elements, then allow threads to achieve the monitor.
-        if (q.size() == qSize) notifyAll();
-        q.offer(integer);
+    synchronized public void insert(Event e) throws InterruptedException {
+        //if the queue has a certain amount of elements, then allowing threads to achieve the monitor.
+        while (q.size() > 20) wait();
+        q.offer(e);
     }
 
-    synchronized public Integer fetch() throws InterruptedException {
+    synchronized public Event fetch() throws InterruptedException {
         //if queue is empty, threads waiting
-        if (q.isEmpty()) wait();
+        while (q.isEmpty()) notifyAll();
+
         return q.poll();
     }
 
@@ -37,56 +55,67 @@ class DataStore {
 
 class Consumer implements Runnable {
     private DataStore dataStore;
+    private int sequence;
 
-    public Consumer(DataStore dataStore) {
+    public Consumer(DataStore dataStore, int sequence) {
         this.dataStore = dataStore;
+        this.sequence = sequence;
     }
 
     @Override
     public void run() {
-        Integer x = null;
-        try {
-            x = this.dataStore.fetch();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (true) {
+            Event x = null;
+            try {
+                x = dataStore.fetch();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Consumer " + " i :" + Thread.currentThread().getName() + " event: " + x);
+
         }
-        System.out.println("fetched task: " + x);
     }
 }
 
 class Producer implements Runnable {
     private DataStore dataStore;
+    private String title = "producer: ";
 
-    public Producer(DataStore dataStore) {
+    public Producer(DataStore dataStore, int sequence) {
         this.dataStore = dataStore;
+        this.title += sequence;
     }
+
 
     @Override
     public void run() {
-
+        try {
+            dataStore.insert(new Event(this.title));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
 public class SuspendThread {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //simulating to suspend a thread.
         DataStore dataStore = new DataStore();
-        Producer producer = new Producer(dataStore);
-        Producer producer1 = new Producer(dataStore);
-        Producer producer2 = new Producer(dataStore);
-        Producer producer3 = new Producer(dataStore);
-        Producer producer4 = new Producer(dataStore);
-        Producer producer5 = new Producer(dataStore);
 
-        Consumer consumer1 = new Consumer(dataStore);
-        Consumer consumer2 = new Consumer(dataStore);
-        Consumer consumer3 = new Consumer(dataStore);
-        Consumer consumer4 = new Consumer(dataStore);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(6);
+        IntStream.rangeClosed(0, 12).forEach(i -> executorService.submit(new Producer(dataStore, i)));
 
 
+        IntStream.range(0, 4).forEach(i -> executorService.submit(new Consumer(dataStore, i)));
+
+        executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+
+        executorService.shutdown();
+        System.out.println("executor service shut down? " + executorService.isShutdown());
+
+        System.exit(1);
     }
 
 }
