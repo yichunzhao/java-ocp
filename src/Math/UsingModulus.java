7890e016a6e35e9f620c4d8927dac1f9d5e18b0c
yichunzhao <zhaoyichun@hotmail.com>
2020-09-22 17:59:23 +0200

    updating demo using modulus

---
+++ src/Math/UsingModulus.java
@@ -0,0 +1,29 @@
+package Math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * given int x and y, finding out times x over y and x reminder x over y
+ */
+public class UsingModulus {
+
+    public static void main(String[] args) {
+        System.out.println(findTimesAndReminder(5, 3));
+        System.out.println(findTimesAndReminder(7, 4));
+        System.out.println(findTimesAndReminder(8, 3));
+        try {
+            System.out.println(findTimesAndReminder(2, 3));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static Map<String, Integer> findTimesAndReminder(Integer x, Integer y) {
+        if (x < y) throw new IllegalArgumentException("x must greater than y");
+        Map<String, Integer> result = new HashMap<>();
+        result.put("reminder", x % y);
+        result.put("times", x / y);
+        return result;
+    }
+}
