799bce74b8462d2c926853c61b0ff3860955b6a1
yichunzhao <zhaoyichun@hotmail.com>
2020-09-22 23:13:34 +0200

    updating use cases of using Arrays.sort()

--- src/OneDimArray/ArySorting.java
+++ src/OneDimArray/ArySorting.java
@@ -3,46 +3,60 @@
  * return int
  * <0 a<b
  * =0 a=b
- * <0 a>b
+ * >0 a>b
  */
 package OneDimArray;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-/** @author YNZ */
+/**
+ * Natural ordering
+ *
+ * @author YNZ
+ */
 class Person implements Comparable<Person> {
 
-  String name;
-  int age;
+    private String name;
+    private int age;
 
-  public Person(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  @Override
-  public int compareTo(Person o) {
-    return this.age - o.age;
-  }
+    public String getName() {
+        return name;
+    }
 
-  @Override
-  public String toString() {
-    return this.name + " " + this.age + " ";
-  }
+    @Override
+    public int compareTo(Person p) {
+        return this.age - p.age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.age + " ";
+    }
 }
 
 public class ArySorting {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Person p1 = new Person("Shreya", 32);
-    Person p2 = new Person("Harry", 40);
-    Person p3 = new Person("Paul", 30);
+        Person p1 = new Person("Shara", 32);
+        Person p2 = new Person("Harry", 40);
+        Person p3 = new Person("Paul", 30);
+        Person p4 = new Person("Mike", 18);
 
-    Person[] persons = {p1, p2, p3};
+        Person[] persons = {p1, p2, p3, p4};
 
-    Arrays.sort(persons);
+        //sorting persons by their natural ordering
+        Arrays.sort(persons);
+        System.out.println("" + Arrays.toString(persons));
 
-    System.out.println("" + Arrays.toString(persons));
-  }
+        System.out.println("then sorting by name: ");
+        Arrays.sort(persons, Comparator.comparing(person -> person.getName()));
+        System.out.println("after sorting by name: " + Arrays.toString(persons));
+    }
 }
