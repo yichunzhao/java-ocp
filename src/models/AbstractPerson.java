e76e23d8a60aea8a86caab26a453ce4196fe2a22
yichunzhao <zhaoyichun@hotmail.com>
2020-09-24 21:11:33 +0200

    Abstraction class and its constructor

---
+++ src/models/AbstractPerson.java
@@ -0,0 +1,28 @@
+package models;
+
+
+public abstract class AbstractPerson {
+    private String firstName;
+    private String lastName;
+
+    public AbstractPerson(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
