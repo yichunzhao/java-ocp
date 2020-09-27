36d675b20b9453c5a16d8a49f8dced8322169232
yichunzhao <zhaoyichun@hotmail.com>
2020-09-27 18:17:45 +0200

    TreeSet and TreeMap implement Navigable interface

---
+++ src/Collection/Map/MapDescendingOrder.java
@@ -0,0 +1,22 @@
+package Collection.Map;
+
+import java.util.Arrays;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+/**
+ * tree implements NavigableMap interface, which offer descendingOrder api.
+ */
+public class MapDescendingOrder {
+
+    public static void main(String[] args) {
+
+        //Map<String, Integer> stringLengthMap =
+        NavigableMap<String, Integer> stringLengthMap = Arrays.stream("we like java programming".split("\\s"))
+                .collect(Collectors.toMap(s -> s, s -> s.length(), (s1, s2) -> s2, TreeMap::new));
+        System.out.println("original order:" + stringLengthMap);
+        System.out.println("descending order:" + stringLengthMap.descendingMap());
+
+    }
+}
