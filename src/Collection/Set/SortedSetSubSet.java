2188a034ee5495d6bd56b9cd80efa9de2daa5806
yichunzhao <zhaoyichun@hotmail.com>
2020-09-27 19:36:32 +0200

    TreeSet<E> is a sortedSet<E>; subset api etc

---
+++ src/Collection/Set/SortedSetSubSet.java
@@ -0,0 +1,25 @@
+package Collection.Set;
+
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * TreeSet<E> IA-A sorted set, subSet(E fromElement, E toElement); fromElement is inclusive; and toElement(exclusive)
+ * tailSet(E fromElement) >= fromElement
+ * headSet(E toElement) < toElement
+ */
+public class SortedSetSubSet {
+
+    public static void main(String[] args) {
+
+        SortedSet<String> sortedStrings = Arrays.stream("we have a meeting today".split("\\s"))
+                .collect(Collectors.toCollection(TreeSet::new));
+        System.out.println("sorted Strings: " + sortedStrings);
+
+        System.out.println("from element b to element w: " + sortedStrings.subSet("b", "w"));
+        System.out.println("tail set from b: " + sortedStrings.tailSet("b"));
+        System.out.println("head set from t: " + sortedStrings.headSet("t"));
+    }
+}
