3d4865f8a5caeffbd7ada4f68653c7a98bd2d7e3
yichunzhao <zhaoyichun@hotmail.com>
2020-09-29 23:19:14 +0200

    add comment

--- src/StreamExp/ConvertPrimArrayIntoList.java
+++ src/StreamExp/ConvertPrimArrayIntoList.java
@@ -9,25 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/** @author YNZ */
+/**
+ * Converting a char array char[] to List<Character>. It cannot directly use Arrays.asList(T... args), for the Type
+ * parameter T will be inferred as char[].
+ * <p>
+ * The easiest way to use Java 8 String to IntStream, and then mapToObject Character. You need cast a int to Character.
+ *
+ * @author YNZ
+ */
 public class ConvertPrimArrayIntoList {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    // a primitive array
-    char[] chars = "I hate it. ".toCharArray();
+        List<Character> charList = new ArrayList<>();
 
-    //
-    List<Character> charList = new ArrayList<>();
-    for (char c : chars) {
-      charList.add(c);
+        // a primitive array
+        char[] chars = "I dislike it.".toCharArray();
+        for (char c : chars) {
+            charList.add(c);
+        }
+
+        // in java 8
+        String str = "what is happening?";
+        List<Character> listOfChar = str.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        System.out.println("List chars : " + listOfChar);
     }
-
-    System.out.println("size of charList " + charList.size());
-
-    // in java 8
-    String str = "what is happening? ";
-    List<Character> listOfChar = str.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-    System.out.println("List chars : " + listOfChar);
-  }
 }
