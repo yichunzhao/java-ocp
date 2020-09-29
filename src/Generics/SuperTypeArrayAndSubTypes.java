df255ab77dbc5fa23e5e155ebac344866d579d84
yichunzhao <zhaoyichun@hotmail.com>
2020-09-29 20:02:08 +0200

    Arrays.asList is a generic method; it method argument generic type is inferred from its return type.

--- src/Generics/SuperTypeArrayAndSubTypes.java
+++ src/Generics/SuperTypeArrayAndSubTypes.java
@@ -11,13 +11,19 @@ import java.util.List;
 public class SuperTypeArrayAndSubTypes {
 
     public static void main(String[] args) {
+
         Integer[] numbers = {10, 2, 3};
         System.out.println(Arrays.toString(inputNumbers(numbers)));
+        Double[] doubles = {10.1, 11.2, 13.5};
+        System.out.println(Arrays.toString(doubles));
 
         //reference generic type must identical to its object generic type;
         List<Integer> numbs = new ArrayList<>(); //a list of Integers objects
         //the following doesn't work! compiling error.
         //inputNumbers(numbs);//method argument:  List<Number> numbers
+        System.out.println(inputNumbers(Arrays.asList(1, 2, 3)));
+        //this is a generic method; the type is influenced from
+        //the return type, namely Number; that is why it works. 
     }
 
     private static Number[] inputNumbers(Number[] numbers) {
