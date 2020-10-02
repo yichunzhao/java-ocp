1cbd4e670eadf772df8175ec83bea6d0c7fd8e2c
yichunzhao <zhaoyichun@hotmail.com>
2020-10-02 02:05:23 +0200

    List<?> can hold any type of elements, once assigned, then cannot be modified.

--- src/Generics/GenericWildCard.java
+++ src/Generics/GenericWildCard.java
@@ -1,18 +1,25 @@
 package Generics;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * List<?> anyStuffs, wild card generic type means you can put any type in the collection.
- * Any type can be assigned to this List, but it cannot be modified.
+ * Any type can be assigned to this List, but the List cannot be modified.
+ * anyStuffs.add(new Integer(4));
+ * this will generate an compiling error
  */
 public class GenericWildCard {
 
     public static void main(String[] args) {
-        List<?> stuffs = new ArrayList<>();
+        List<?> stuffs = Arrays.asList(1, LocalDate.now());
+        System.out.println("stuffs:" + stuffs);
+        //once the List is assigned elements, it cannot be modified later on. Somehow, like having applied a final
+        //keyword. 
+        //the following gives an compiling error.
+        //stuffs.add(LocalDate.of(2020,10,1));
         input(stuffs);
-
     }
 
     public static void input(List<?> anyStuffs) {
