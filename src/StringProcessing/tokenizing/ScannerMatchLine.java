126d69e5aa94859f172cecf27d0d50c22af6618b
yichunzhao <zhaoyichun@hotmail.com>
2020-10-08 15:45:39 +0200

    Scanner.findLine fits a String to a pattern; when scanner retrieve method receives a mis-matched type from the token, it throws java.util.InputMismatchException

--- src/StringProcessing/tokenizing/ScannerMatchLine.java
+++ src/StringProcessing/tokenizing/ScannerMatchLine.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 /**
  * Scanner.findLine(regEx) matching the specified pattern in an entire line.
+ * <p>
+ * when received token type doesn't match the method type to retrieve the data; it throws java.util.InputMismatchException
  */
 public class ScannerMatchLine {
 
@@ -20,5 +22,20 @@ public class ScannerMatchLine {
 
         //if the string is found, return it.  if the string not matched, returns null
         System.out.println("find a string in a line: " + scanner.findInLine("Paul 10"));
+
+        String str = "1 a 10 . 100 1000";
+        Scanner scanner1 = new Scanner(str);
+
+        try {
+            int total = 0;
+
+            for (int i = 0; i < 4; i++) {
+                total += scanner1.nextInt();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
