1f506eb8be47f8f780fb0bdef3538386d726d6a6
yichunzhao <zhaoyichun@hotmail.com>
2020-10-10 12:55:35 +0200

    wordBoundary

---
+++ src/StringProcessing/regex/WordBoundary.java
@@ -0,0 +1,23 @@
+package StringProcessing.regex;
+
+import java.util.Arrays;
+
+/**
+ * what is word boundary?  regEx:  "\\b"; each word, space, and comma starting with a word boundary.
+ */
+public class WordBoundary {
+
+    public static void main(String[] args) {
+        String target = "amita, matinda,shreya,mike, and anthony are arrogant";
+        Arrays.stream(target.split("\\b")).forEach(s -> System.out.println(s));
+
+        System.out.println("size: " + target.split("\\b").length);
+
+        //replace the first letter of all words with A.
+
+        //replaceAll("regEx",replacement)
+        System.out.printf("replace the first letter with A: %s \n", target.replaceAll("\\b\\w", "A"));
+        //replace(charSequence literal, CharSequence literal)
+        System.out.printf("replace the first letter with A: %s \n", target.replace("\\b\\w", "A"));
+    }
+}
