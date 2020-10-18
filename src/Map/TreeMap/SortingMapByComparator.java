432c98a33a4270165ee36492a7c331fb58d5da3b
yichunzhao <zhaoyichun@hotmail.com>
2020-10-18 02:04:57 +0200

    sorting a hashmap using its stream and a comparator.

---
+++ src/Map/TreeMap/SortingMapByComparator.java
@@ -0,0 +1,33 @@
+package Map.TreeMap;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Sorting map by an external comparator without key merging.
+ */
+public class SortingMapByComparator {
+    private static String target = "If another class wants to create an instance of class Inner";
+
+    public static void main(String[] args) {
+        //splitting a string into words
+        List<String> strings = Arrays.stream(target.split("\\s")).collect(toList());
+        System.out.println(strings);
+
+        //collecting strings into a map
+        Map<String, Integer> map = strings.stream()
+                .collect(Collectors.toMap(s -> s, s -> s.length(), (o, n) -> n));
+        System.out.println("string-length-map: " + map);
+
+        //sorting by word length without overlapping
+        Comparator<Map.Entry<String, Integer>> comparator = Comparator.comparing(Map.Entry::getValue);
+        List<Map.Entry<String, Integer>> sortedMap = map.entrySet().stream().sorted(comparator.reversed()).collect(Collectors.toList());
+        System.out.println("sorted string-length-map by length" + sortedMap);
+    }
+
+}
