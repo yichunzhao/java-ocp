c24d065d5c038906fd8d5cea3c435b018651504a
yichunzhao <zhaoyichun@hotmail.com>
2020-10-30 15:22:49 +0100

    java modulus when x<y, then modulus = x

--- src/Math/UsingModulus.java
+++ src/Math/UsingModulus.java
@@ -4,23 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Java has one arithmetical operator, %, also known as the remainder or modulus
+ * <p>
  * given int x and y, finding out times x over y and reminder x over y
+ * <p>
+ * if x<y remainder = x
  */
 public class UsingModulus {
 
     public static void main(String[] args) {
+        System.out.println(findTimesAndReminder(11, 7));
+        System.out.println(findTimesAndReminder(1, 7));
+        System.out.println(findTimesAndReminder(1120, 7));
         System.out.println(findTimesAndReminder(5, 3));
         System.out.println(findTimesAndReminder(7, 4));
         System.out.println(findTimesAndReminder(8, 3));
-        try {
-            System.out.println(findTimesAndReminder(2, 3));
-        } catch (Exception e) {
-            System.out.println(e);
-        }
     }
 
     public static Map<String, Integer> findTimesAndReminder(Integer x, Integer y) {
-        if (x < y) throw new IllegalArgumentException("x must greater than y");
         Map<String, Integer> result = new HashMap<>();
         result.put("reminder", x % y);
         result.put("times", x / y);
