2da6d5d1d77fbd5eff67e365a69cb5a6356249bc
yichunzhao <zhaoyichun@hotmail.com>
2020-11-07 19:43:32 +0100

    containsKey() or containsValue() depends on the method hashcode() and equalTo()

--- src/Collection/Map/MapContains.java
+++ src/Collection/Map/MapContains.java
@@ -7,12 +7,31 @@ import java.util.stream.Collectors;
 
 /**
  * Map contains, it returns ture and false
+ * <p>
+ * boolean containsKey(key): using hashcode() and equalTo()
+ * boolean containsValue(value): using equalTo()
+ * <p>
+ * You need to watch out here, if both methods have been overridden correctly.
  */
+class Emp {
+    private String name;
+
+    public Emp(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof String)) return false;
+        return name.equals((String) o);
+    }
+}
+
 public class MapContains {
 
     public static void main(String[] args) {
         Map<String, Integer> stringLengthMap = Arrays.stream("we like java programing".split("\\s"))
-                .collect(Collectors.toMap(s -> s, s -> s.length(), (s1, s2) -> s2, HashMap::new));
+                .collect(Collectors.toMap(s -> s, String::length, (s1, s2) -> s2, HashMap::new));
 
         System.out.println("contains we? " + stringLengthMap.containsKey("we"));
         System.out.println("contains 2? " + stringLengthMap.containsValue(2));
@@ -20,7 +39,12 @@ public class MapContains {
         System.out.println("contains value xxx? " + stringLengthMap.containsValue("xxx"));
         System.out.println("contains key yyy? " + stringLengthMap.containsValue("yyy"));
 
-        //watch out NullPointerException here
-        System.out.println("get key that is not existed: " + stringLengthMap.get("xxx"));
+        Map<Emp, Emp> empEmpMap = new HashMap<>();
+        empEmpMap.put(new Emp("mike"), new Emp("shara"));
+        //You find the original emp mike, for the hashcode decide the bucket index
+        System.out.println(empEmpMap.containsKey(new Emp("mike")));
+        //You can find this shara
+        System.out.println(empEmpMap.containsValue(new Emp("shara")));
+
     }
 }
