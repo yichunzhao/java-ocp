284f0f2bb0dd5ac6c3743c0cda1fa948df4105b5
yichunzhao <zhaoyichun@hotmail.com>
2020-11-08 01:05:17 +0100

    more on Arrays sort

--- src/Array/SortArray.java
+++ src/Array/SortArray.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
+/**
+ * Sorting arrays
+ * <p>
+ * Using helper Arrays
+ * sort(byte[] a); sort(byte[], int fromIndex, int toIndex);
+ * sort(int[] a); sort(int[] a, int fromIndex, int toIndex);
+ * sort(Object[] a); sort(Object[], fromIndex, int toIndex);
+ * sort(T[] a, Comparator<? super T> c)
+ * sort(T[] a, Comparator<? super T> c, int fromIndex, int toIndex);
+ * <p>
+ * Object array without implement Comparable interface will throw a Runtime exception.
+ */
 class Person {
     private String name;
     private String phone;
@@ -63,6 +75,12 @@ public class SortArray {
 
         Person[] persons = {new Person("mike", "12345678"), new Person("peter", "22223333"), new Person("Jack", "44556677")};
         System.out.println("un-sorted" + Arrays.toString(persons));
+
+        try {
+            Arrays.sort(persons);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         Arrays.sort(persons, Comparator.comparing(Person::getName));
         System.out.println("sorted: " + Arrays.toString(persons));
