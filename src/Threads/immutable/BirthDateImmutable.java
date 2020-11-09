fc0d43da8970ba20c627644bee8496d4c7c4f8ef
yichunzhao <zhaoyichun@hotmail.com>
2020-11-09 01:37:09 +0100

    immutable object is thread-safe.

---
+++ src/Threads/immutable/BirthDateImmutable.java
@@ -0,0 +1,47 @@
+package Threads.immutable;
+
+/**
+ * Immutable objects are thread-safe; for its states cannot be modified.
+ * <p>
+ * 1.final class that cannot be extended, so that it cannot be overridden to modify the states.
+ * 2.private variables so that states cannot be manipulated externally.
+ * 3.final variables preventing from being re-assigned.
+ * 4. return a clone of value, so that modification via reference impossible.
+ */
+
+import java.util.Date;
+
+final class BirthDate {
+    private final Date birth;
+
+    public BirthDate(Date birth) {
+        this.birth = birth;
+    }
+
+    public final Date getBirthDate() {
+        return (Date) birth.clone();
+    }
+
+    public boolean isOlder(Date other) {
+        boolean isOlder = birth.before(other);
+        return isOlder;
+    }
+
+}
+
+public class BirthDateImmutable {
+
+    public static void main(String[] args) {
+        BirthDate birthDate = new BirthDate(new Date());
+        System.out.println("original date: " + birthDate.getBirthDate());
+
+        Date another = birthDate.getBirthDate();
+        System.out.println("another: "+ another);
+        another.setTime(2001);
+        System.out.println("modifying another: "+ another);
+        System.out.println("birth-date after modification: " + birthDate.getBirthDate());
+
+    }
+}
+
+
