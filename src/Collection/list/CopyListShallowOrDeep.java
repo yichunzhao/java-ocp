2c34bdcab8f952b4778cc4bedd9673187545ca74
yichunzhao <zhaoyichun@hotmail.com>
2021-02-16 18:13:29 +0100

    updating comment

--- src/Collection/list/CopyListShallowOrDeep.java
+++ src/Collection/list/CopyListShallowOrDeep.java
@@ -3,53 +3,48 @@ package Collection.list;
 import models.Name;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 /**
- * easy way to make a copy of a list is to use a constructor
+ * A shallow copy of a List
  * <p>
- * question: is this a shallow copy or a deep copy?
+ * an easy way to make a copy of a list is to use a constructor
  * <p>
- * it is a shallow copy. two List share the same element references
+ * it is a shallow copy. two List share the same references points to elements
  * <p>
- * meanwhile, addAll method is the same as the constructor method, copy of list and list both hold the same references
- * to the element instances. if you change on one side, another side is also changed.
+ * List.addAll method is the same as the constructor, sharing the same references to the element instances.
+ * if you make a change on one list, the content in another list is modified too.
+ * <p>
+ * <p>
+ * When using a shallow copy?
+ * when need to resort a list without any modifications; but don't want to pollute the origin.
+ * you may use a shallow copy.
  */
 public class CopyListShallowOrDeep {
 
     public static void main(String[] args) {
         Name name = Name.of("Kathy", "Sierra");
         Name name1 = Name.of("Bert", "Bates");
-        List<Name> names = Stream.of(name, name1).collect(toList());
+
+        List<Name> names = new ArrayList<>();
+        names.add(name);
+        names.add(name1);
+
         System.out.println("names: " + names);
 
         List<Name> copyOfNames = new ArrayList<>(names);
         System.out.println("copy of names: " + copyOfNames);
 
-        //make a change on the copy
+        System.out.println("make a change on the copy...");
+
         copyOfNames.get(0).setFirstName("Mike");
 
         //both list having been modified
         System.out.println("modified copy: " + copyOfNames);
-        System.out.println("original names: " + copyOfNames);
+        System.out.println("original names: " + names);
 
         //watch out here:
         System.out.println("copy contains original? " + copyOfNames.containsAll(names));
-
-        /**
-         * deep copy of a list
-         */
-        //source size should <= dest size; otherwise it throws exception
-        //so simple way to construct a shallow copy.
-        List<Name> dest = new ArrayList<>(names);
-        //then making a deep copy, using Collections static method
-        Collections.copy(dest, names);
-
-        //now, dest List should contain different Name instances from the Original one.
-        System.out.println("dest contains all of names? " + dest.contains(names));
     }
+
 }
