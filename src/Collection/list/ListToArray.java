e2ff2bc9495dd3fa491490e6f3b18d1271a108da
yichunzhao <zhaoyichun@hotmail.com>
2021-03-22 09:17:02 +0100

    updating Integer list streaming to int[]

--- src/Collection/list/ListToArray.java
+++ src/Collection/list/ListToArray.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Converting a List to an array
- * *
+ * Converting a List of Integer to an array, fx: Integer[] or int[]
+ *
  * <T> T[] List.toArray(T[] array)
  */
 public class ListToArray {
@@ -21,9 +21,19 @@ public class ListToArray {
         Integer[] integerArray = integerList.toArray(new Integer[0]);
         System.out.println("integerArray: " + Arrays.toString(integerArray));
 
-        //java 8, streaming to array
-        Integer[] anotherArray = integerList.stream().toArray(Integer[]::new);
-        System.out.println("anotherArray: " + Arrays.toString(anotherArray));
+        //converting Integer list to Integer[]
+        //java 8, streaming <A> A[] toArray(IntFunction<A[]> var1)
+        Integer[] another = integerList.stream().toArray((Integer[]::new));
+        System.out.println("anotherArray: " + Arrays.toString(another));
+
+        //Arrays.asList(T... e) watch out here type param. T
+        List<Integer> listsIntegers = Arrays.asList(another);
+        System.out.println("integer list: " + listsIntegers);
+
+        //map each Integer to int
+        //Integer stream to int[];   ToIntFunction<? super T> var1
+        int[] intNumbs = integerList.stream().mapToInt(x -> x).toArray();
+        System.out.println("converting Integer list to int[] : " + Arrays.toString(intNumbs));
     }
 
 }
