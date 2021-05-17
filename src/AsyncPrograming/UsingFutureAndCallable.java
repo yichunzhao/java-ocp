b1759b15d6aa2bdcbcdddfa82716a6ad672fa9c7
yichunzhao <zhaoyichun@hotmail.com>
2021-05-17 02:35:11 +0200

    carrying out Future Callable by ExecutorService

---
+++ src/AsyncPrograming/UsingFutureAndCallable.java
@@ -0,0 +1,49 @@
+package AsyncPrograming;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class UsingFutureAndCallable {
+
+    private static class MyTask implements Callable<Long> {
+        @Override
+        public Long call() throws InterruptedException {
+            Thread.sleep(10);
+            return 1997L;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Main thread: " + Thread.currentThread().getName());
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Long> futureResult = executorService.submit(new MyTask());
+
+        System.out.println("get the result in the future...");
+
+        while (!futureResult.isDone()) {
+            System.out.println("I am doing something else");
+        }
+
+        try {
+            System.out.println("final result: " + futureResult.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
+        if (!executorService.isShutdown()) {
+            try {
+                executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
