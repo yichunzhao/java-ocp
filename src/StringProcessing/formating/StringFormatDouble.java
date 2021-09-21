f1bc0b9da4ee7b49e897b5be72f3cdff7d4130f2
yichunzhao <zhaoyichun@hotmail.com>
2021-09-21 21:14:54 +0200

    formatting a double to N decimals

--- src/StringProcessing/formating/StringFormatDouble.java
+++ src/StringProcessing/formating/StringFormatDouble.java
@@ -6,5 +6,9 @@ public class StringFormatDouble {
         double n = 1.567D;
         String formatted = String.format("%.1f", n);
         System.out.println("formatted: " + formatted);
+
+        float m = 3.567f;
+        String formattedFloat = String.format("%.1f", m);
+        System.out.println("formatted float: " + formattedFloat);
     }
 }
