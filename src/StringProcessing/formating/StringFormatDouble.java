e68643f2f446743ab6e557b6c1754ad313a5aaa2
yichunzhao <zhaoyichun@hotmail.com>
2021-09-21 21:28:52 +0200

    using BigDecimal formatting a double to N decimals

--- src/StringProcessing/formating/StringFormatDouble.java
+++ src/StringProcessing/formating/StringFormatDouble.java
@@ -1,5 +1,7 @@
 package StringProcessing.formating;
 
+import java.math.BigDecimal;
+
 public class StringFormatDouble {
 
     public static void main(String[] args) {
@@ -10,5 +12,8 @@ public class StringFormatDouble {
         float m = 3.567f;
         String formattedFloat = String.format("%.1f", m);
         System.out.println("formatted float: " + formattedFloat);
+
+        double tmp = BigDecimal.valueOf(n).setScale(1, BigDecimal.ROUND_HALF_UP).doubleValue();
+        System.out.println("after round up: " + tmp);
     }
 }
