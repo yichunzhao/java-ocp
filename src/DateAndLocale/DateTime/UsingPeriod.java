0974c816c47584ad72fc724febf50119e1a91501
yichunzhao <zhaoyichun@hotmail.com>
2021-09-27 02:29:49 +0200

    exploring dateTime

--- src/DateAndLocale/DateTime/UsingPeriod.java
+++ src/DateAndLocale/DateTime/UsingPeriod.java
@@ -4,14 +4,23 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 
-/**
- * Period is used to represent a time span between too dates.
- */
+/** Period is used to represent a time span between too dates. */
 public class UsingPeriod {
 
-    public static void main(String[] args) {
-        Period p = Period.between(LocalDate.of(2019, Month.APRIL, 01), LocalDate.of(2020, Month.AUGUST, 31));
-        System.out.println("Period between two dates " + p.getYears() + " years " + p.getMonths() + " months " + p.getDays() + " days");
-    }
+  public static void main(String[] args) {
+    Period p =
+        Period.between(LocalDate.of(2019, Month.APRIL, 01), LocalDate.of(2020, Month.AUGUST, 31));
+    System.out.println(
+        "Period between two dates "
+            + p.getYears()
+            + " years "
+            + p.getMonths()
+            + " months "
+            + p.getDays()
+            + " days");
 
+    // from period to create period
+    var p_ = Period.from(p);
+    System.out.printf("from period to period %s ", p_.getMonths());
+  }
 }
--- src/DateAndLocale/Java8DateTime/days/HandlingDays.java
+++ src/DateAndLocale/Java8DateTime/days/HandlingDays.java
@@ -1,23 +1,31 @@
 package DateAndLocale.Java8DateTime.days;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class HandlingDays {
 
-    public static void main(String[] args) {
-        LocalDate date = LocalDate.now();
-        System.out.printf("current date %s \n", date);
+  public static void main(String[] args) {
+    LocalDate date = LocalDate.now();
+    System.out.printf("current date %s \n", date);
 
-        LocalDate startOfJun = LocalDate.of(2021, 6, 1);
-        System.out.printf("1st day minus 5 day: %s \n", startOfJun.minusDays(5));
+    LocalDate startOfJun = LocalDate.of(2021, 6, 1);
+    System.out.printf("1st day minus 5 day: %s \n", startOfJun.minusDays(5));
 
-        System.out.printf("get the day of month of StartOfJun: %s \n", startOfJun.getDayOfMonth());
-        System.out.printf("get the day of month of StartOfJun and minus 5: %s \n", startOfJun.getDayOfMonth() - 5);
+    System.out.printf("get the day of month of StartOfJun: %s \n", startOfJun.getDayOfMonth());
+    System.out.printf(
+        "get the day of month of StartOfJun and minus 5: %s \n", startOfJun.getDayOfMonth() - 5);
 
-        LocalTime time = LocalTime.now();
-        System.out.printf("current time : %s \n ", time);
+    LocalTime time = LocalTime.now();
+    System.out.printf("current time : %s \n ", time);
 
-    }
+    var isWeekend = isWeekend(LocalDate.of(2021, 9, 26));
+    System.out.printf("Is a weekend? %b ", isWeekend);
+  }
 
+  public static boolean isWeekend(LocalDate date) {
+    return date.getDayOfWeek().equals(DayOfWeek.SATURDAY)
+        || date.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+  }
 }
