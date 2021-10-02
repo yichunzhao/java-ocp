fd8e7a864d78015697897593fdbabb8a75768165
yichunzhao <zhaoyichun@hotmail.com>
2021-10-03 00:09:23 +0200

    Overloaded Collectors.partitioningBy() with Collector as second parameter

--- src/functionalProgramming/Lamada/collector/PartitionOfNumbers.java
+++ src/functionalProgramming/Lamada/collector/PartitionOfNumbers.java
@@ -7,6 +7,8 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.counting;
+
 public class PartitionOfNumbers {
 
   public static void main(String[] args) {
@@ -17,12 +19,16 @@ public class PartitionOfNumbers {
     IntStream.rangeClosed(0, 20).forEach(i -> numbers.add(rand.nextInt(30)));
 
     System.out.printf("numbers %s \n", numbers);
-
     System.out.println("partition: >12");
-
     Map<Boolean, List<Integer>> parts =
         numbers.stream().collect(Collectors.partitioningBy(n -> n > 12));
     System.out.printf("number >12: %s \n", parts.get(true));
     System.out.printf("number <=12: %s \n", parts.get(false));
+
+    System.out.println("Overloaded partitionBy method by the second parameter.");
+
+    var booleanSizeMap =
+        numbers.stream().collect(Collectors.partitioningBy(n -> n > 12, counting()));
+    System.out.printf("size of >12 numbers: %s \n", booleanSizeMap.get(true));
   }
 }
