028aec784f40017f451f525c4ea0e8423767f7c4
yichunzhao <zhaoyichun@hotmail.com>
2021-10-03 20:15:53 +0200

    removing a key-value from a map

---
+++ src/Collection/Map/HashMap/DeleteKeyValuePair.java
@@ -0,0 +1,22 @@
+package Collection.Map.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * delete a key-value from a map, just by removing a key.
+ */
+public class DeleteKeyValuePair {
+  public static void main(String[] args) {
+    Map<Integer, String> map = new HashMap<>();
+    map.put(1, "Beijing");
+    map.put(2, "Shanghai");
+    map.put(3, "HongKong");
+    map.put(4, "Pairs");
+    map.put(5, "Berlin");
+    System.out.println("full map" + map);
+
+    map.keySet().remove(1);
+    System.out.println("after removal operation:" + map);
+  }
+}
