9fbc7468e2159e35fbef3e7a7192502fc58451ca
yichunzhao <zhaoyichun@hotmail.com>
2021-10-04 00:00:25 +0200

    removing a key-value from a map using a predict

--- src/Collection/Map/HashMap/DeleteKeyValuePair.java
+++ src/Collection/Map/HashMap/DeleteKeyValuePair.java
@@ -3,9 +3,7 @@ package Collection.Map.HashMap;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * delete a key-value from a map, just by removing a key.
- */
+/** delete a key-value from a map, just by removing a key. */
 public class DeleteKeyValuePair {
   public static void main(String[] args) {
     Map<Integer, String> map = new HashMap<>();
@@ -18,5 +16,8 @@ public class DeleteKeyValuePair {
 
     map.keySet().remove(1);
     System.out.println("after removal operation:" + map);
+
+    map.keySet().removeIf(i -> i == 3);
+    System.out.println("after removal operation: " + map);
   }
 }
